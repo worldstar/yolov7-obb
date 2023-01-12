f71800078bb83954c7e67691d536233c86b2582c
Supasin Wuthikulphakdi <49264553+Suppersine@users.noreply.github.com>
2023-01-12 11:19:01 +0800

    Added opt.noval to the parser.add_argument block
    Added opt.noval to the parser.add_argument block

--- train.py
+++ train.py
@@ -545,6 +545,7 @@ if __name__ == '__main__':
     parser.add_argument('--rect', action='store_true', help='rectangular training')
     parser.add_argument('--resume', nargs='?', const=True, default=False, help='resume most recent training')
     parser.add_argument('--nosave', action='store_true', help='only save final checkpoint')
+    parser.add_argument('--noval', action='store_true', help='only validate final epoch')
     parser.add_argument('--notest', action='store_true', help='only test final epoch')
     parser.add_argument('--noautoanchor', action='store_true', help='disable autoanchor check')
     parser.add_argument('--evolve', action='store_true', help='evolve hyperparameters')
